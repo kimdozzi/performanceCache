6e3af9d58623dddf1733610caadfbf82fbd87cb1
kimdozzi <neo5188@gmail.com>
2024-06-09 18:56:48 +0900

    first commit

---
+++ src/main/java/com/ll/performancecache/PerformanceCacheApplication.java
@@ -0,0 +1,13 @@
+package com.ll.performancecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/performancecache/PerformanceCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.performancecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

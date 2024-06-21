f68ef389d27f83558adb1b07ad0045baf4155ab1
kimdozzi <neo5188@gmail.com>
2024-06-21 15:37:49 +0900

    Ngrinder를 이용한 성능 개선 확인

--- src/main/java/com/ll/performancecache/service/NoticeServiceImpl.java
+++ src/main/java/com/ll/performancecache/service/NoticeServiceImpl.java
@@ -5,29 +5,28 @@ import com.ll.performancecache.mapper.NoticeReadMapper;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class NoticeServiceImpl implements NoticeService{
+public class NoticeServiceImpl implements NoticeService {
 
     private final NoticeReadMapper noticeReadMapper;
 
-    public NoticeServiceImpl(NoticeReadMapper noticeReadMapper){
+    public NoticeServiceImpl(NoticeReadMapper noticeReadMapper) {
         this.noticeReadMapper = noticeReadMapper;
     }
 
     @Override
-    @Cacheable(value = "NoticeReadMapper.findAll")
+    // @Cacheable(value = "NoticeReadMapper.findAll")
     public List<Notice> getAllNotices() {
         return noticeReadMapper.findAll();
     }
 
     @Override
-    @Cacheable(value = "NoticeReadMapper.findByPage", key = "#request.requestURI + '-' + #pageNumber", condition = "#pageNumber <= 5")
+    // @Cacheable(value = "NoticeReadMapper.findByPage", key = "#request.requestURI + '-' + #pageNumber", condition = "#pageNumber <= 5")
     public List<Notice> findByPage(HttpServletRequest request, int pageNumber) {
         int startIdx = (pageNumber - 1) * 10;
         return noticeReadMapper.findByPage(startIdx);
